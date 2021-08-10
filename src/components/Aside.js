d77a00ea0047ae050549b793b4e04ec8667af60a
andrew bostick <andbostick@gmail.com>
2021-08-09 20:48:23 -0400

    connected tailwind/ estabished aside component

---
+++ src/components/Aside.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Aside() {
+    
+    
+    
+    return (
+        <div className="h-screen w-1/4 bg-gray-300 lg:w-20 xl:w-20">
+            <h1>HEllo from aside</h1>    
+        </div>
+        
+    )
+}
+
+export default Aside;
